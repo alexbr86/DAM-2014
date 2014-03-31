6174d337b5449895b1d4d8fc95cb9d1346c9d8a2
alexbr86 <abalseiro001@gmail.com>
2014-03-31 19:32:15 +0200

    HTML5

---
+++ HTML5/ej02/js/main.js
@@ -0,0 +1,16 @@
+﻿$(function(){
+    'use strict';
+
+    document.desingMode= 'on';
+
+    $(document).on('click', '#btn-bold', function(e){
+        document.execCommand('bold',false,null);
+    });
+    $(document).on('click', '#btn-itlc', function(e){
+        document.execCommand('italic',false,null);
+    });
+    $(document).on('click', '#btn-line', function(e){
+        document.execCommand('underline',false,null);
+    });
+
+});
