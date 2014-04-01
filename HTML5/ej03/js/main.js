20bf8d4b971a90d93702c85746405e1c03cc8d8a
alexbr86 <abalseiro001@gmail.com>
2014-04-01 19:32:46 +0200

    HTML5

---
+++ HTML5/ej03/js/main.js
@@ -0,0 +1,10 @@
+﻿$(function(){
+    var $progress = $('progress');
+    $(document).on('blur', 'input', function(e){
+        var $this = $(this);
+        if ($this.val().length>0){
+            $progress.val($progress[0].value+1);
+            console.log($progress[0].value);
+        }
+    });
+});
---
+++ HTML5/ej03/js/modernizr.js
@@ -0,0 +1,26 @@
+﻿$(function(){
+    
+    for (var i in Modernizr.inputtypes){
+        if(Modernizr.inputtypes[i]===false){
+            $('input[type="'+ i + '"]').addClass('disabled');
+        }
+        
+
+    }
+    $div= $('<div/>',{
+        'class' : 'disabled'
+    });
+    for (var j in Modernizr.video){
+        if(Modernizr.video[j].length===0){
+            var str = j + ':' + Modernizr.video[j]; 
+            $div.append( str);
+        }
+        
+
+    }
+    var $form = $('form');
+$div.insertAfter($form);
+            
+        
+    
+});
