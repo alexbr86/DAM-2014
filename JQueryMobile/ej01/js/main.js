34b8f2077d6bd045734c4774c2e1e52a434bdea0
alexbr86 <abalseiro001@gmail.com>
2014-04-10 19:15:55 +0200

    JQueryMobile

---
+++ JQueryMobile/ej01/js/main.js
@@ -0,0 +1,12 @@
+﻿$(function(){
+
+
+    $('body').bind('swipe', '#main', function(e){
+        $('#header').html("<h1>SWIPE!!!</h1>" );
+        
+        
+    });
+    $(window).bind('orientationchange', function(e){
+        $('#header').html( "<h1>This device is in " + e.orientation + " mode!</h1>" );
+    });
+});
